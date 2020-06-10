1ae68a2c044b3ff8d773158d159da7c75e2bd219
Eder Sena <eder.sena@live.com>
2020-06-10 13:53:31 -0300

    Movie Overview component and project wrap up

--- moviedb-finder/src/components/App.js
+++ moviedb-finder/src/components/App.js
@@ -1,44 +1,73 @@
 import React, { Component } from 'react';
+import '../App.css';
 import Nav from './Nav'
-import SearchArea from './SearchArea'
+import SearchBox from './SearchBox'
 import MovieList from './MovieList'
+import Pagination from './Pagination'
+import MovieInfo from './MovieInfo';
 
-const apikey = process.env.REACT_APP_API
-
+const apiKey = process.env.REACT_APP_API
 class App extends Component {
-  constructor() {
+  constructor(){
     super()
     this.state = {
       movies: [],
-      searchTerm: ''
+      totalResults: 0,
+      searchTerm: '',
+      currentPage: 1,
+      currentMovie: null
     }
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apikey}&query=${this.state.searchTerm}`)
-    .then(data => data.json())
-    .then(data => {
-      console.log(data);
-      this.setState({ movies: [...data.results]})
-    })
   }
 
   handleChange = (e) => {
     this.setState({ searchTerm: e.target.value })
   }
 
+  logData = () => {
+    console.log(process.env.REACT_APP_API);
+  }
+
+  handleSubmit = (e) => { 
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${this.state.searchTerm}&language=en-US&page=${this.state.currentPage}`)
+    .then(data => data.json())
+    .then(data => {
+      this.setState({ movies: [...data.results], totalResults: data.total_results})
+    })
+    
+    e.preventDefault()
+  }
+
+  nextPage = (pageNumber) => {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${this.state.searchTerm}&language=en-US&page=${pageNumber}`)
+    .then(data => data.json())
+    .then(data => {
+      this.setState({ movies: [...data.results], totalResults: data.total_results, currentPage: pageNumber})
+    })
+  }
+
+  viewMovieInfo = (id) => {
+    let filteredMovie;
+    this.state.movies.forEach((movie, i) => {
+      if(movie.id == id) {
+        filteredMovie = movie
+      }
+    }) 
+
+    this.setState({ currentMovie: filteredMovie })  
+  }
+
+  closeMovieInfo = () => {
+    this.setState({ currentMovie: null })
+  }
+
   render() {
+    let numberPages = Math.floor(this.state.totalResults / 20);
     return (
-      <div className="App">
-        <Nav />
-        <SearchArea
-          handleSubmit={this.handleSubmit}
-          handleChange={this.handleChange}
-        />
-        <MovieList movies={this.state.movies}/>
-      </div>
+        <div>
+          <Nav />
+          {this.state.currentMovie == null ? <div><SearchBox handleSubmit={this.handleSubmit} handleChange={this.handleChange}/><MovieList viewMovieInfo={this.viewMovieInfo} movies={this.state.movies}/></div> : <MovieInfo closeMovieInfo={this.closeMovieInfo} currentMovie={this.state.currentMovie} />}
+            {this.state.totalResults > 20 && this.state.currentMovie == null ? <Pagination pages={numberPages} nextPage={this.nextPage} currentPage={this.state.currentPage}/> : ''}
+        </div>
     );
   }
 }
